6a1a4d22fee10274ba2e55377a9e9c0841550f4f
Fatemeh <fatemehfaryadras7@gmail.com>
2023-10-27 23:58:07 +0330

    add some details

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import { sumFromTo, getAverage, sort } from "./components/list-calculator.js";
 import getRandomNumbers from "./components/get-random-numbers.js";
 
-const numberList = [5, 10, 15, 20];
-
-const start = 5;
-const end = 10;
-const resultSumFromTo = sumFromTo(start, end);
-console.log(`${start} to ${end} = ${resultSumFromTo}`);
-
-const resultGetAverage = getAverage(numberList);
-console.log(`result : ${resultGetAverage}`);
-
-const sortedList = sort(numberList);
-console.log(`sortedList :${sortedList}`);
+// const numberList = [5, 10, 15, 20];
 
 const count = 5;
 const randomNumbers = getRandomNumbers(count);
 console.log("randomNumbers:", randomNumbers);
+
+var minNumber = Math.min(...randomNumbers);
+var maxNumber = Math.max(...randomNumbers);
+
+const start = minNumber;
+const end = maxNumber;
+const resultSumFromTo = sumFromTo(start, end);
+console.log(`${start} to ${end} = ${resultSumFromTo}`);
+
+const resultGetAverage = getAverage(randomNumbers);
+console.log(`result : ${resultGetAverage}`);
+
+const sortedList = sort(randomNumbers);
+console.log(`sortedList :${sortedList}`);
